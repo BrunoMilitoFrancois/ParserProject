fbb9283299165fbcf53727b1cad21e7461b77617
Bruno François <bruno.francois@zbra.com.br>
2017-11-10 16:09:38 -0200

    Added some classes

---
+++ ParserProject/ParserProject/Sum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParserProject
+{
+    class Sum : IOperator
+    {
+        public int Calculate(Number a, Number b)
+        {
+            return a.Value + b.Value;
+        }
+    }
+}
