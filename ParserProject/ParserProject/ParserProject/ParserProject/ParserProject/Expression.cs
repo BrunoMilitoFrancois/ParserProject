b320f66735744d7f76106f81c5ff7033b0aaaec2
Bruno François <30418568+brunofrancoisZBRA@users.noreply.github.com>
2017-11-12 23:45:42 -0200

    Add files via upload

---
+++ ParserProject/ParserProject/ParserProject/ParserProject/ParserProject/Expression.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParserProject
+{
+    public class Expression
+    {
+        Expression Parse(string formula)
+        {
+            return null;
+        }
+    }
+}
---
+++ ParserProject/ParserProject/ParserProject/ParserProject/ParserProject/IOperator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParserProject
+{
+    public interface IOperator
+    {
+        int Calculate(Number a, Number b);
+    }
+}
